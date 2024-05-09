a3d6f63c4906c98c27a2b7e6b9e6abff47c3511b
Ondrejsed <156373959+Targothh@users.noreply.github.com>
2024-05-09 14:55:36 +0200

    Update lcd_handler.h

--- lcd_handler.h
+++ lcd_handler.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "objects.h"
 uint16_t * buffer_init();
+void draw_pixel(uint16_t * buffer, int x, int y, uint16_t data);
 void draw(uint16_t buffer, unsigned char *parlcd_mem_base);
 
